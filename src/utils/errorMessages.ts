62f801dc519648ec17b71dd35421107c037f8adb
arturpietrzak <artur.pietrzak1@outlook.com>
2023-07-09 15:58:31 +0200

    Add error messages

---
+++ src/utils/errorMessages.ts
@@ -0,0 +1,5 @@
+export const errorMessages = {
+  UNAUTHORIZED: "You need to sign in to perform this action",
+  FORBIDDEN: "You do not have sufficient permissions to perform this action",
+  BAD_REQUEST: "There was an issue with your action",
+};
